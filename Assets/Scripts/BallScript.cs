456360cdce35ad8d446fc2ac5e302a83fa5e24fc
IhorProgrammer <ihor.kuzubov@gmail.com>
2024-06-22 18:38:21 +0300

    Initial commit

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    public float force = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fx = Input.GetAxis("Horizontal") * Time.timeScale * force;
+        float fz = Input.GetAxis("Vertical") * Time.timeScale * force;
+
+
+
+        // rb.AddForce(fx, 0, fz); - за світовими координатами, незалежно від камери 
+        // якщо потрібно обертати камеру, то слід використовувати її вектори 
+        // forward тa right
+
+        Vector3 camForward = Camera.main.transform.forward; 
+        camForward.y = 0f;
+        camForward = camForward.normalized;
+        
+        Vector3 moveDirection = fz * camForward + fx * Camera.main.transform.right;
+        rigidbody.AddForce(moveDirection);
+    }
+}
---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    private GameObject ball;
+    private Vector3 offset; // зміщення камери відносно персонажу
+    private Vector3 mAngles;
+
+    private float sensitivityH = 2f;
+    private float sensitivityV = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.ball = GameObject.Find("Ball");
+        this.offset = this.transform.position - ball.transform.position;
+        mAngles = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mAngles.y += Input.GetAxis("Mouse X") * sensitivityH;
+        mAngles.x -= Input.GetAxis("Mouse Y") * sensitivityV;
+
+
+        if (mAngles.x > 75f) mAngles.x = 75f;  
+        if (mAngles.x < 5f) mAngles.x = 5f;
+
+        if (mAngles.y > 360) mAngles.y -= 360; 
+        if (mAngles.y < 0) mAngles.y += 360;
+    }
+
+    private void LateUpdate()
+    {
+        // вплив на камеру краще робити в LateUpdate
+        this.transform.position = (ball.transform.position) 
+            + Quaternion.Euler( 0, mAngles.y, 0 )  * offset;
+        this.transform.eulerAngles = mAngles;
+    }
+}
